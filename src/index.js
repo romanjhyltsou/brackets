87aa672241834893998f6dd70e62f4b15aabd51f
romanjhyltsou <romanjhyltsou@gmail.com>
2022-07-18 20:14:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let bracketsArrToStr = bracketsConfig.join('').replace(/,/g, ''); // преобразовать массив в строку и убираем точки
+  let sArr = [];
+
+  for (let bracket of str) {
+
+
+      let bracketsIn = bracketsArrToStr.indexOf(bracket);
+
+
+      if (bracketsIn % 2 === 0) {
+          
+          if (bracket === bracketsArrToStr[bracketsIn + 1] && sArr[sArr.length - 1] === bracketsIn){
+              sArr.pop();
+          } else if (bracket === bracketsArrToStr[bracketsIn + 1] && sArr[sArr.length - 1] !== bracketsIn) {
+              sArr.push(bracketsIn);
+          }
+          else{
+              sArr.push(bracketsIn);
+          }
+
+      }else {
+          if (sArr.pop() !== bracketsIn -1){
+            return false;
+          }
+      }
+      
+  }
+  return sArr.length === 0;
+};
